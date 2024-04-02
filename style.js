205cf73f899819a582cf83144d806abd9088095f
Will99919 <danjouloic@live.fr>
2024-04-02 11:22:08 +0200

    update

--- style.js
+++ style.js
@@ -1,18 +1,3 @@
-/* $('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
-});
- */
-
-// JavaScript pour ajouter une action au bouton "Réserver maintenant"
-/* document.getElementById('reserveBtn').addEventListener('click', function() {
-    alert('Vous avez réservé votre trajet !');
-}); */
-
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
